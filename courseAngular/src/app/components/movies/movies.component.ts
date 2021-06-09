3f5a475d837139d834b2e61331868cdc16df0c37
Yeyner Arenales <jeiner.arenales0811@gmail.com>
2021-06-09 12:05:00 -0400

    created model, and slider in components

--- courseAngular/src/app/components/movies/movies.component.ts
+++ courseAngular/src/app/components/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-movies',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  movies: Array<Movie> = [];
+
+  constructor() {
+    this.movies = [
+      new Movie(2019, "Spiderman", "https://sm.ign.com/ign_es/screenshot/default/maxresdefault-29_ccvn.jpg"),
+      new Movie(2020, "Avengers: EndGame", "https://lumiere-a.akamaihd.net/v1/images/image_1e5c5703.jpeg?region=0%2C0%2C540%2C810"),
+      new Movie(2017, "Justice League", "https://images-na.ssl-images-amazon.com/images/I/91JNWWQKGgL._RI_.jpg")
+    ]
+  }
 
   ngOnInit(): void {
   }
